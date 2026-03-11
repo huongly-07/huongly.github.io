a4653323c8edefa365e9fd2b0705d46f53e64829
huongly-07 <25112069@st.vju.ac.vn>
2026-03-11 23:59:41 +0700

    Add files via upload

---
+++ b3.py
@@ -0,0 +1,18 @@
+horizontal = h = "+ - - - - + - - - - +"
+vertical =v = "|         |         |"
+print (h)
+for i in range(4):
+    print (v)
+print (h)
+for i in range(4):
+     print (v)
+print (h)
+h2 = "+ - - - - + - - - - + - - - - + - - - - +"
+v2 = "|         |         |         |         |"
+print (h2)
+for i in range(4):
+    print (v2)
+print (h2)
+for i in range(4):
+    print (v2)
+print (h2)
