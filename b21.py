cc58db646ccaece499875ae50784e3330f340963
huongly-07 <25112069@st.vju.ac.vn>
2026-04-02 12:17:13 +0700

    Update b21.py

--- b21.py
+++ b21.py
@@ -17,8 +17,8 @@ print("B",(x,y))
 print("C",(-x,-y))
 
 import math
-distanceBO = math.sqrt(x^2 + y^2)
+distanceBO = math.sqrt(x**2 + y**2)
 print("Distance from B to O: ",distanceBO)
 
-distanceAB = math.sqrt((x-A.x)^2+(y-A.y)^2)
+distanceAB = math.sqrt((x-A.x)**2+(y-A.y)**2)
 print("Distance from A to B: ",distanceAB)
